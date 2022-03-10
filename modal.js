e022ab19c6c0091aa6ba866e000718faaa5dcfea
Everton Martins <3532everton@gmail.com>
2022-03-10 14:53:04 -0300

    adição do model

--- modal.js
+++ modal.js
@@ -2,7 +2,6 @@
 function abrirModal(projeto){
     const infor = document.getElementById(`informacoes${projeto}`)
     infor.style.display = 'block'
-    infor.style.transition = '1s'
     infor.animate([
         {opacity: '0'},
         {opacity: '1'}
